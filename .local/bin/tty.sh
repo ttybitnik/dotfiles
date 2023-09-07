8709818642e63020045b192cb9a96c5aa93bd1a8
ttybitnik <vinicius.moraes@eternodevir.com>
2023-09-07 17:47:24 -0300

    feat(tty.sh): add tty.sh library

---
+++ .local/bin/tty.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Personal library with sugar functions.
+#   Prefix:
+#     tty
+
+#===============================================================================
+# Display message and exit properly for errors.
+#-------------------------------------------------------------------------------
+# Arguments:
+#   Error message to display
+# Outputs:
+#   Error message to stderr
+# Returns:
+#   Non-zero due erratic behaviour
+#===============================================================================
+tty::err() {
+    printf "%s\n" "$@" >&2
+    exit 1
+}
+
+#===============================================================================
+# Run command explicitly, for debugging or displaying the command.
+#-------------------------------------------------------------------------------
+# Arguments:
+#   Command to run
+# Outputs:
+#   Command run, with a prefix highlighter, to stdout
+# Returns:
+#   Zero if command was run, non-zero on error
+#===============================================================================
+tty::run_print() {
+    printf "\n>> %s \n\n" "$*"
+    "$@"
+}
