ea0f8eaa08385ac448bd3a41e9bac5c0db1a4dee
ttybitnik <vinicius.moraes@eternodevir.com>
2023-11-09 21:59:35 -0300

    feat(sway): implement new autostart config

--- .config/sway/autostart.sh
+++ .config/sway/autostart.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#===============================================================================
+#				   WARNING
+#-------------------------------------------------------------------------------
+# This file is deprecated since 2023-11-09.
+# Now the autostart.sh flow is dealt directly in Sway config file.
+#===============================================================================
 gpg-agent --daemon &
 gammastep -l "$TTY_GEO" &
 sleep 5
